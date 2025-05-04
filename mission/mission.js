b0bd4a948198ac05528b53c5fdd0092383ecea68
estebansar <sar17008@byui.edu>
2025-05-03 22:16:29 -0600

    last changes

--- mission/mission.js
+++ mission/mission.js
@@ -1,8 +1,8 @@
-const themeOptions = document.getElementById('theme-select');
+const themeSelector = document.getElementById('theme-select');
 const body1 = document.querySelector('body')
 const byuiLogo = document.querySelector('.logo_webp');
-function changeBackground() {
-    const selectedBackground = themeOptions.value;
+function changeTheme() {
+    const selectedBackground = themeSelector.value;
     if (selectedBackground == 'dark') {
         body1.classList.add('dark');
         byuiLogo.setAttribute('src', 'images/byui-logo_white.png');
@@ -13,4 +13,4 @@ function changeBackground() {
     }
 }
 
-themeOptions.addEventListener('change', changeBackground);
+themeSelector.addEventListener('change', changeTheme);
