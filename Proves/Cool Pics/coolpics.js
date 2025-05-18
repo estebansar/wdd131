86392d9612ace4fc3393eae24d493e228e5183b9
estebansar <sar17008@byui.edu>
2025-05-18 02:31:09 -0600

    problem with the css, I kept display:none... removed it and now it works!!

--- Proves/Cool Pics/coolpics.js
+++ Proves/Cool Pics/coolpics.js
@@ -1,31 +1,33 @@
-const menuButton = document.querySelector("#menu");
-const nav = document.querySelector("nav");
-menuButton.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.querySelector("#menu");
+  const nav = document.querySelector("nav");
+  menuButton.addEventListener("click", function () {
     nav.classList.toggle("hide");
-});
+  });
 
-const gallery = document.querySelector(".gallery");
-const viewer = document.querySelector(".view-hide");
+  const gallery = document.querySelector(".gallery");
+  console.log("gallery is:", gallery);
+  const viewer = document.querySelector(".view-hide");
 
-
-function viewerTemplate(src,alt) {
-    return '<img src="' + src + '" alt="' + alt + '">' +
-           '<button class="close">x</button>';
+  function viewerTemplate(src, alt) {
+  return '<img src="' + src + '" alt="' + alt + '">' +
+         '<button class="close">x</button>';
 }
 
-
-window.addEventListener("resize", function() {
+  window.addEventListener("resize", function () {
     if (window.innerWidth > 1000) {
-        nav.classList.remove("hide");
+      nav.classList.remove("hide");
     } else {
-        nav.classList.add("hide");
+      nav.classList.add("hide");
     }
-});
+  });
 
-gallery.addEventListener("click", function(event) {
+  gallery.addEventListener("click", function (event) {
     const clicked = event.target;
+    console.log("Clicked:", clicked)
 
     if (clicked.tagName !== "IMG") {
+        console.log("Not an image");
         return;
     }
 
@@ -33,22 +35,27 @@ gallery.addEventListener("click", function(event) {
     const imgText = clicked.getAttribute("alt");
     const fullPic = smallPic.replace("-sm", "-full");
 
+     console.log("smallPic:", smallPic);
+     console.log("fullPic:", fullPic);
+     console.log("alt text:", imgText);
+
     viewer.innerHTML = viewerTemplate(fullPic, imgText);
     viewer.classList.remove("hide");
-});
+  });
 
-viewer.addEventListener("click", function(event) {
+  viewer.addEventListener("click", function (event) {
     const clicked = event.target;
-
     if (clicked.classList.contains("close") || clicked === viewer) {
-        viewer.classList.add("hide");
+      viewer.classList.add("hide");
     }
+  });
+
+  if (window.innerWidth > 1000) {
+    nav.classList.remove("hide");
+  } else {
+    nav.classList.add("hide");
+  }
 });
 
-if (window.innerWidth > 1000) {
-    nav.classList.remove("hide");
-}   else {
-    nav.classList.add("hide");
-}
 
 /*Had to copy/paste JS after VS Code wouldn't save — force-closed and reopened the file.*/
