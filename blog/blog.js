b793fec73a8dec1dcbe57a6bf8c395ad569dfe6f
estebansar <sar17008@byui.edu>
2025-05-31 15:37:52 -0600

    added cs to the project

--- blog/blog.js
+++ blog/blog.js
@@ -2,7 +2,7 @@ const articles = [
 	{
 		id: 1,
 		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
+		date: 'May 31, 2025',
 		description:
 			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
 		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
@@ -23,5 +23,41 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
-]
+];
+
+function articleTemplate(article) {
+	return `
+		<article>
+			<div class="details">
+				<p class="date">${article.date}</p>
+				<p>Age: ${article.ages}</p>
+				<p>Genre: ${article.genre}</p>
+			</div>
+			<div class="content">
+				<h2>${article.title}</h2>
+				<img src="${article.imgSrc}" alt="${article.imgAlt}">
+				<p>${article.description}</p>
+			</div>
+		</article>
+	`;
+
+}
+
+const output = document.querySelector(".articles");
+articles.forEach(article => {
+	output.innerHTML += articleTemplate(article);
+});
